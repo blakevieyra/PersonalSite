648b57335ce20ef2434b4a050f01c1cb3ccbaf76
blakevieyra <88246090+OMNIPOTENT15@users.noreply.github.com>
2024-04-21 18:24:48 -0700

    adding node

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './html/src/home.js', // Corrected entry point
+    entry: './src/home.js', // Corrected entry point
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
